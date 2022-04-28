4376dceb2344a4b8ce1b89ea5c8ebb903944f03d
Michael Ehrenreich <michideep@gmail.com>
2022-04-28 03:43:51 +0200

    Organize defines in ab_boot.h better

--- ab_boot/ab_boot.h
+++ ab_boot/ab_boot.h
@@ -2,12 +2,18 @@
 
 #include <stdint.h>
 
-#define CONFIG_START 0x0803f800
 #define FLASH_START  0x08000000
-#define FLASH_END    0x08040000
+#define AB_BOOT_SIZE 0x00002000
 
 #define APP_A_START  0x08002000
 #define APP_B_START  0x08020800
+                     // 122 KiB
+#define APP_SIZE        0x1e800
+
+#define CONFIG_START 0x0803f800
+#define CONFIG_SIZE       0x800
+
+#define FLASH_END    0x08040000
 
 struct ab_boot_config
 {
